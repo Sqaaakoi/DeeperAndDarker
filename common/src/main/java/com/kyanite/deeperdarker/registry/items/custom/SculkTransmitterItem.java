40e93108dc1bd6e0d7c9867c3ae1d5b7198b9ce2
Sqaaakoi <sqaaakoi@sqaaakoi.xyz>
2023-01-16 03:05:06 +1300

    Make Sculk Transmitter work across different dimensions

--- common/src/main/java/com/kyanite/deeperdarker/registry/items/custom/SculkTransmitterItem.java
+++ common/src/main/java/com/kyanite/deeperdarker/registry/items/custom/SculkTransmitterItem.java
@@ -1,10 +1,17 @@
 package com.kyanite.deeperdarker.registry.items.custom;
 
+import java.util.List;
+
 import com.kyanite.deeperdarker.miscellaneous.DDTags;
 import com.kyanite.deeperdarker.registry.sounds.DDSounds;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
@@ -17,10 +24,9 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
+import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
-
-import java.util.List;
 
 public class SculkTransmitterItem extends Item {
     public SculkTransmitterItem(Properties pProperties) {
@@ -29,47 +35,58 @@ public class SculkTransmitterItem extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        if(isLinked(pPlayer.getItemInHand(pUsedHand))) {
-            transmit(pLevel, pPlayer, pUsedHand);
+        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+        if(isLinked(stack)) {
+            if (pLevel.isClientSide()) {
+                pPlayer.playSound(DDSounds.SCULK_TRANSMIT.get(), 0.5f, pLevel.getRandom().nextFloat() * 0.4F + 0.8F);
+                return InteractionResultHolder.success(stack);
+            }
+            transmit(getLinkedDimension(stack, pLevel.getServer()), pPlayer, pUsedHand);
+            return InteractionResultHolder.sidedSuccess(pPlayer.getItemInHand(pUsedHand), pLevel.isClientSide());
         }
-
+        
         return super.use(pLevel, pPlayer, pUsedHand);
     }
 
     @Override
     public InteractionResult useOn(UseOnContext pContext) {
-        if(isLinked(pContext.getItemInHand())) {
-            return transmit(pContext.getLevel(), pContext.getPlayer(), pContext.getHand());
+        if (isLinked(pContext.getItemInHand())) {
+            if (pContext.getLevel().isClientSide()) {
+                pContext.getPlayer().playSound(DDSounds.SCULK_TRANSMIT.get(), 0.5f, pContext.getLevel().getRandom().nextFloat() * 0.4F + 0.8F);
+                return InteractionResult.SUCCESS;
+            }
+            transmit(getLinkedDimension(pContext.getItemInHand(), pContext.getPlayer().getServer()), pContext.getPlayer(), pContext.getHand());
+            return InteractionResult.CONSUME;
         }
 
-        if(!pContext.getLevel().getBlockState(pContext.getClickedPos()).is(DDTags.Blocks.TRANSMITTABLE)) {
+        if (!pContext.getLevel().getBlockState(pContext.getClickedPos()).is(DDTags.Blocks.TRANSMITTABLE)) {
             pContext.getPlayer().displayClientMessage(Component.translatable("item.deeperdarker.sculk_transmitter.not_container"), true);
             return InteractionResult.FAIL;
         }
 
-        if(!isLinked(pContext.getItemInHand())) {
+        if (!isLinked(pContext.getItemInHand())) {
             pContext.getPlayer().playSound(DDSounds.SCULK_LINK.get(), 0.5f, pContext.getLevel().getRandom().nextFloat() * 0.4F + 0.8F);
             setBlock(pContext.getItemInHand(), pContext.getPlayer(), pContext.getHand(), pContext.getClickedPos());
-            return InteractionResult.SUCCESS;
+            return InteractionResult.sidedSuccess(pContext.getLevel().isClientSide());
         }
 
-        return super.useOn(pContext);
+        return InteractionResult.sidedSuccess(pContext.getLevel().isClientSide());
     }
 
-    public InteractionResult transmit(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+    public boolean transmit(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
         BlockPos linked = getLinkedBlockPos(pPlayer.getItemInHand(pUsedHand));
         if(isLinked(pPlayer.getItemInHand(pUsedHand))) {
             BlockState state = pLevel.getBlockState(linked);
 
             if(state == null || linked == null || pPlayer.isCrouching()) {
                 setBlock(pPlayer.getItemInHand(pUsedHand), pPlayer, pUsedHand, null);
-                return InteractionResult.FAIL;
+                return false;
             }
 
             if(!pLevel.getBlockState(linked).is(DDTags.Blocks.TRANSMITTABLE)) {
                 pPlayer.displayClientMessage(Component.translatable("item.deeperdarker.sculk_transmitter.not_found"), true);
                 setBlock(pPlayer.getItemInHand(pUsedHand), pPlayer, pUsedHand, null);
-                return InteractionResult.FAIL;
+                return false;
             }
 
             pPlayer.playSound(DDSounds.SCULK_TRANSMIT.get(), 0.5f, pLevel.getRandom().nextFloat() * 0.4F + 0.8F);
@@ -83,17 +100,18 @@ public class SculkTransmitterItem extends Item {
 
             if(menuProvider != null) {
                 pPlayer.openMenu(menuProvider);
-
                 BlockEntity blockEntity = pLevel.getBlockEntity(linked);
-                if(blockEntity instanceof ChestBlockEntity chestBlockEntity) {
+                if (blockEntity instanceof ChestBlockEntity chestBlockEntity) {
                     chestBlockEntity.startOpen(pPlayer);
+                } else if (blockEntity instanceof ShulkerBoxBlockEntity shulkerBoxBlockEntity) {
+                    shulkerBoxBlockEntity.startOpen(pPlayer);
                 }
             }
 
-            return InteractionResult.SUCCESS;
+            return true;
         }
 
-        return InteractionResult.FAIL;
+        return false;
     }
 
     public BlockPos getLinkedBlockPos(ItemStack stack) {
@@ -107,6 +125,14 @@ public class SculkTransmitterItem extends Item {
         }
 
         return null;
+    }
+
+    public Level getLinkedDimension(ItemStack stack, MinecraftServer server) {
+        if(!stack.hasTag()) server.getLevel(Level.OVERWORLD);
+        if(stack.getTag().contains("dimension")) {
+            return server.getLevel(ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(stack.getTag().getString("dimension"))));
+        }
+        return server.getLevel(Level.OVERWORLD);
     }
 
     public boolean isLinked(ItemStack stack) {
@@ -125,11 +151,13 @@ public class SculkTransmitterItem extends Item {
         if(pos == null) {
             setModelData(stack, player, hand, 0);
             tag.remove("linked");
+            tag.remove("dimension");
             player.getItemInHand(hand).setTag(tag);
             return;
         }
 
         tag.putIntArray("linked", new int[]{pos.getX(), pos.getY(), pos.getZ()});
+        tag.putString("dimension", player.getLevel().dimension().location().toString());
         setModelData(stack, player, hand, 1);
         player.getItemInHand(hand).setTag(tag);
     }
